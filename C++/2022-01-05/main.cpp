2b8b9e3dace95d1c776969e1f08ad31383c3ee8d
dtdat <dtdat@humaxdigital.com>
2022-01-05 13:33:21 +0700

    Create folder

---
+++ C++/2022-01-05/main.cpp
@@ -0,0 +1,9 @@
+#include <cstdlib>
+#include "stdio.h"
+
+using namespace std; 
+
+int main()
+{
+	return EXIT_SUCCESS; 
+}
---
+++ C++/makeFolder.bat
@@ -0,0 +1,44 @@
+:: Make new folder day by day
+
+@echo off
+IF #%1#==## (GOTO NORMAL) ELSE (GOTO SPECIAL)
+
+:NORMAL
+set "fname=%DATE:~-4%-%DATE:~-10,2%-%DATE:~-7,2%"
+IF EXIST %fname% GOTO PRINT_MESSAGE
+mkdir %fname%
+copy "./run.bat" "%fname%/run.bat"
+cd %fname%
+echo #include ^<cstdlib^>> main.cpp
+echo #include "stdio.h">> main.cpp
+echo.>> main.cpp
+echo using namespace std; >> main.cpp
+echo.>> main.cpp
+echo int main()>> main.cpp
+echo {>> main.cpp
+echo 	return EXIT_SUCCESS; >> main.cpp
+echo }>> main.cpp
+
+GOTO EXIT
+
+:SPECIAL
+set "fname=%1"
+IF EXIST %fname% GOTO PRINT_MESSAGE
+mkdir %fname%
+copy "./run.bat" "%fname%/run.bat"
+cd %fname%
+echo #include ^<cstdlib^>> main.cpp
+echo #include "stdio.h">> main.cpp
+echo.>> main.cpp
+echo using namespace std; >> main.cpp
+echo.>> main.cpp
+echo int main()>> main.cpp
+echo {>> main.cpp
+echo 	return EXIT_SUCCESS; >> main.cpp
+echo }>> main.cpp
+
+GOTO EXIT
+
+:PRINT_MESSAGE
+ECHO Folder exist!
+:EXIT
