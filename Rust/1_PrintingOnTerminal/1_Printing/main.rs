04a229ab922e82107dff337b9a002ad1d5261123
Rakuhouha 10 <rakauhouha10@gmail.com>
2023-02-18 01:16:17 +0700

    Update Rust Rust

--- Rust/1_PrintingOnTerminal/1_Printing/main.rs
+++ Rust/1_PrintingOnTerminal/1_Printing/main.rs
@@ -1,7 +1,13 @@
 fn main() {
-	let a = 100;
-	// a = 10; // (1. if uncommad this line)
-	let mut b = 1000;
-	b = 10; // (2. if uncommad this line)
-	println!("a = {}, b = {}", a, b);
+	let constant_value = 0;
+	// 'let' creat constant value, that mean the value can not be change
+	println!("Let start the constant_value = {}", constant_value);
+	// if need value could be change, go with 'let mut' (mutable)
+	let mut non_constant_value = 10;
+	println!("Init non_constant_value = {}", non_constant_value);
+	println!("Then change non_constant_value value to 20");
+	non_constant_value += 20;
+	println!("New non_constant_value = {}", non_constant_value);
+	// Also, we can call directly like the name the follow
+	println!("Constant_value = {constant_value}, non_constant_value = {non_constant_value}");
 }
