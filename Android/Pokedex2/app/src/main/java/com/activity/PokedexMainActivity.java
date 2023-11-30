9b963dc5c733b7f0f705417d756e8471b33e3a1b
dtdat <rakauhouha10@gmail.com>
2023-12-01 03:32:30 +0700

    Start new Pokedex app get content from .csv file

---
+++ Android/Pokedex2/app/src/main/java/com/activity/PokedexMainActivity.java
@@ -0,0 +1,14 @@
+package com.activity.pokedex2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PokedexMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokedex_main);
+    }
+}
