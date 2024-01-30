9b14f819afcc82dd6800a649d8d86cf01bb97104
Tad_DatDT <dtdat@humaxdigital.com>
2024-01-30 19:58:49 +0700

    [Web] Electron copy file/folder app
    	Part 1: Design UI and select file/folder

--- Web/ElectronApp/CopyFilesApp/preload.js
+++ Web/ElectronApp/CopyFilesApp/preload.js
@@ -5,39 +5,121 @@
  *
  * https://www.electronjs.org/docs/latest/tutorial/sandbox
  */
-function createButton (bname, btype) {
-  // 1. Create the button
-  var button = document.createElement('button');
-  button.className = btype;
-  button.innerHTML = bname;
-  button.setAttribute("width", 200);
-  // 2. Append somewhere
-  var body = document.getElementsByTagName("body")[0];
-  body.appendChild(button);
-
-  // 3. Add event handler
-  button.addEventListener ("click",
-    function() {
-      alert("did something");
-    }
-  );
+var selected;
+var disp;
+function importFile() {
+  let input = document.createElement('input');
+  input.type = 'file';
+  input.onchange = _ => {
+    // you can use this method to get file and perform respective operations
+    const files = Array.from(input.files);
+    console.log(files);
+    console.log(files[0].path);
+    const ip = document.getElementById("source_dir");
+    ip.value = files[0].path
+  };
+  input.click();
 }
 
-function cpu() {
-  var os = require('os');
-  alert(os.cpus().valueOf());
+async function importFolder() {
+  let input = document.createElement('input');
+  input.type = 'file';
+  input.innerText += "webkitdirectory";
+  input.innerText += "directory";
+  input.onchange = _ => {
+    // you can use this method to get file and perform respective operations
+    const files = Array.from(input.files);
+    console.log(files);
+    console.log(files[0].path);
+    // const ip = document.getElementById("source_dir");
+    // ip.value = files[0].path
+  };
+  input.click();
 }
+
+function alertSomething() {
+  alert("did something");
+}
+
+function show(a, b)
+{
+  console.log(a + "; " + b);
+  // document.getElementById(selected).style.backgroundColor = "rgb(150,150,150)";
+  document.getElementById(disp).style.display = "none";
+  // document.getElementById(a).style.backgroundColor = "rgb(200,200,200)";
+  document.getElementById(b).style.display = "block";
+  selected=a;
+  disp=b;
+}
+
+function dosomething()
+{
+  console.log(a + "; " + b);
+  // document.getElementById(selected).style.backgroundColor = "rgb(150,150,150)";
+  document.getElementById(target_dir).style.display = "none";
+  // document.getElementById(a).style.backgroundColor = "rgb(200,200,200)";
+  document.getElementById(b).style.display = "block";
+  selected=a;
+  disp=b;
+}
+
 /// Start windows application
 window.addEventListener('DOMContentLoaded', () => {
-  // const replaceText = (selector, text) => {
-  //   const element = document.getElementById(selector)
-  //   if (element) element.innerText = text
-  // }
 
-  // for (const type of ['chrome', 'node', 'electron']) {
-  //   replaceText(`${type}-version`, process.versions[type])
-  // }
+  selected="sel1";
+  disp="resultsel1";
 
-  const button1 = document.getElementById("b1");
-  button1.addEventListener("click", cpu);
+  const button1 = document.getElementById("source_file");
+  button1.addEventListener("click", function() {
+    let input = document.createElement('input');
+    input.type = 'file';
+    input.onchange = _ => {
+      // you can use this method to get file and perform respective operations
+      const files = Array.from(input.files);
+      console.log(files);
+      console.log(files[0].path);
+      button1.innerHTML = files[0].path;
+      button1.style.setProperty("background-color", "#aeffb9");
+      button1.style.setProperty("text-align", "left");
+    };
+    input.click();
+  });
+
+  const button2 = document.getElementById("target_dir");
+  button2.addEventListener("click", function() {
+    let input = document.createElement('input');
+    input.type = 'file';
+    input.setAttribute("webkitdirectory","");
+    input.setAttribute("directory","");
+    input.onchange = _ => {
+      // you can use this method to get file and perform respective operations
+      const files = Array.from(input.files);
+      console.log(files);
+      console.log(files[0].path);
+      filePath = files[0].path;
+      dirPath = filePath.substring(0,filePath.lastIndexOf("\\")+1);
+      button2.innerText = dirPath;
+      button2.style.setProperty("background-color", "#aeffb9");
+      button2.style.setProperty("text-align", "left");
+    };
+    input.click();
+  });
+
+  // const b2 = document.getElementById("btn_copy_file");
+  // b2.addEventListener("click", importFolder);
+
+  // const button2 = document.getElementById("b2");
+  // button2.addEventListener("click", alertSomething);
+
+  // const button3 = document.getElementById("b3");
+  // button3.addEventListener("click", alertSomething);
+
+  const item1 = document.getElementById("sel1");
+  item1.addEventListener("click", function(){show("sel1", "resultsel1")});
+
+  const item2 = document.getElementById("sel2");
+  item2.addEventListener("click", function(){show("sel2", "resultsel2")});
+
+  const item3 = document.getElementById("sel3");
+  item3.addEventListener("click", function(){show("sel3", "resultsel3")});
 })
---
+++ Web/ElectronApp/CopyFilesApp/test.js
@@ -0,0 +1,5 @@
+const dialog = require('node-file-dialog')
+const config={type:'directory'}
+dialog(config)
+    .then(dir => console.log(dir))
+    .catch(err => console.log(err))
